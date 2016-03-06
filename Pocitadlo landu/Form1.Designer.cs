4b077a784607ee98dfe0174cc43bb439b9b1a4ab
Najtwisak <jlerl@atlas.cz>
2016-03-06 15:17:31 +0100

    prvni commit a import zdrojaku

---
+++ Pocitadlo landu/Form1.Designer.cs
@@ -0,0 +1,409 @@
+﻿namespace Pocitadlo_landu
+{
+    partial class d
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PocetKaretVBaliku = new System.Windows.Forms.NumericUpDown();
+            this.PocetLanduVBaliku = new System.Windows.Forms.NumericUpDown();
+            this.PocetKaretVRuce = new System.Windows.Forms.NumericUpDown();
+            this.PocetLanduVRuce = new System.Windows.Forms.NumericUpDown();
+            this.a = new System.Windows.Forms.Label();
+            this.b = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.q = new System.Windows.Forms.Label();
+            this.VypocitatButton = new System.Windows.Forms.Button();
+            this.vysledek1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.vysledek5 = new System.Windows.Forms.Label();
+            this.vysledek4 = new System.Windows.Forms.Label();
+            this.vysledek3 = new System.Windows.Forms.Label();
+            this.vysledek2 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Scry = new System.Windows.Forms.CheckBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.EfectTextBox = new System.Windows.Forms.TextBox();
+            this.PushToStackButton = new System.Windows.Forms.Button();
+            this.StackListView = new System.Windows.Forms.ListView();
+            this.EfectNameColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PopStackButton = new System.Windows.Forms.Button();
+            this.Index = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ResolveAllButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PocetKaretVBaliku)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PocetLanduVBaliku)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PocetKaretVRuce)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PocetLanduVRuce)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PocetKaretVBaliku
+            // 
+            this.PocetKaretVBaliku.Location = new System.Drawing.Point(158, 19);
+            this.PocetKaretVBaliku.Name = "PocetKaretVBaliku";
+            this.PocetKaretVBaliku.Size = new System.Drawing.Size(120, 20);
+            this.PocetKaretVBaliku.TabIndex = 0;
+            // 
+            // PocetLanduVBaliku
+            // 
+            this.PocetLanduVBaliku.Location = new System.Drawing.Point(158, 45);
+            this.PocetLanduVBaliku.Name = "PocetLanduVBaliku";
+            this.PocetLanduVBaliku.Size = new System.Drawing.Size(120, 20);
+            this.PocetLanduVBaliku.TabIndex = 1;
+            // 
+            // PocetKaretVRuce
+            // 
+            this.PocetKaretVRuce.Location = new System.Drawing.Point(158, 71);
+            this.PocetKaretVRuce.Name = "PocetKaretVRuce";
+            this.PocetKaretVRuce.Size = new System.Drawing.Size(120, 20);
+            this.PocetKaretVRuce.TabIndex = 2;
+            // 
+            // PocetLanduVRuce
+            // 
+            this.PocetLanduVRuce.Location = new System.Drawing.Point(158, 97);
+            this.PocetLanduVRuce.Name = "PocetLanduVRuce";
+            this.PocetLanduVRuce.Size = new System.Drawing.Size(120, 20);
+            this.PocetLanduVRuce.TabIndex = 3;
+            // 
+            // a
+            // 
+            this.a.AutoSize = true;
+            this.a.Location = new System.Drawing.Point(6, 16);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(104, 13);
+            this.a.TabIndex = 4;
+            this.a.Text = "Pocet karet v balíku";
+            // 
+            // b
+            // 
+            this.b.AutoSize = true;
+            this.b.Location = new System.Drawing.Point(6, 45);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(104, 13);
+            this.b.TabIndex = 5;
+            this.b.Text = "Pocet landu v baliku";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Pocet karet v ruce";
+            // 
+            // q
+            // 
+            this.q.AutoSize = true;
+            this.q.Location = new System.Drawing.Point(6, 99);
+            this.q.Name = "q";
+            this.q.Size = new System.Drawing.Size(97, 13);
+            this.q.TabIndex = 7;
+            this.q.Text = "Pocet landu v ruce";
+            // 
+            // VypocitatButton
+            // 
+            this.VypocitatButton.Location = new System.Drawing.Point(109, 139);
+            this.VypocitatButton.Name = "VypocitatButton";
+            this.VypocitatButton.Size = new System.Drawing.Size(169, 40);
+            this.VypocitatButton.TabIndex = 8;
+            this.VypocitatButton.Text = "Vypocitat";
+            this.VypocitatButton.UseVisualStyleBackColor = true;
+            this.VypocitatButton.Click += new System.EventHandler(this.VypocitatButton_Click);
+            // 
+            // vysledek1
+            // 
+            this.vysledek1.AutoSize = true;
+            this.vysledek1.Location = new System.Drawing.Point(243, 210);
+            this.vysledek1.Name = "vysledek1";
+            this.vysledek1.Size = new System.Drawing.Size(0, 13);
+            this.vysledek1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 210);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Pravedepodobnost 1. kolo:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Pravedepodobnost 2. kolo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Pravedepodobnost 3. kolo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 295);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Pravedepodobnost 4. kolo:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 323);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Pravedepodobnost 5. kolo:";
+            // 
+            // vysledek5
+            // 
+            this.vysledek5.AutoSize = true;
+            this.vysledek5.Location = new System.Drawing.Point(243, 323);
+            this.vysledek5.Name = "vysledek5";
+            this.vysledek5.Size = new System.Drawing.Size(0, 13);
+            this.vysledek5.TabIndex = 15;
+            // 
+            // vysledek4
+            // 
+            this.vysledek4.AutoSize = true;
+            this.vysledek4.Location = new System.Drawing.Point(243, 295);
+            this.vysledek4.Name = "vysledek4";
+            this.vysledek4.Size = new System.Drawing.Size(0, 13);
+            this.vysledek4.TabIndex = 16;
+            // 
+            // vysledek3
+            // 
+            this.vysledek3.AutoSize = true;
+            this.vysledek3.Location = new System.Drawing.Point(243, 266);
+            this.vysledek3.Name = "vysledek3";
+            this.vysledek3.Size = new System.Drawing.Size(0, 13);
+            this.vysledek3.TabIndex = 17;
+            // 
+            // vysledek2
+            // 
+            this.vysledek2.AutoSize = true;
+            this.vysledek2.Location = new System.Drawing.Point(243, 237);
+            this.vysledek2.Name = "vysledek2";
+            this.vysledek2.Size = new System.Drawing.Size(0, 13);
+            this.vysledek2.TabIndex = 18;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(819, 607);
+            this.tabControl1.TabIndex = 19;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.Scry);
+            this.tabPage1.Controls.Add(this.b);
+            this.tabPage1.Controls.Add(this.vysledek2);
+            this.tabPage1.Controls.Add(this.PocetKaretVBaliku);
+            this.tabPage1.Controls.Add(this.vysledek3);
+            this.tabPage1.Controls.Add(this.PocetLanduVBaliku);
+            this.tabPage1.Controls.Add(this.vysledek4);
+            this.tabPage1.Controls.Add(this.PocetKaretVRuce);
+            this.tabPage1.Controls.Add(this.vysledek5);
+            this.tabPage1.Controls.Add(this.PocetLanduVRuce);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.a);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.q);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.VypocitatButton);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.vysledek1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(811, 581);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Keepneš?";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // Scry
+            // 
+            this.Scry.AutoSize = true;
+            this.Scry.Location = new System.Drawing.Point(9, 139);
+            this.Scry.Name = "Scry";
+            this.Scry.Size = new System.Drawing.Size(47, 17);
+            this.Scry.TabIndex = 19;
+            this.Scry.Text = "Scry";
+            this.Scry.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.ResolveAllButton);
+            this.tabPage2.Controls.Add(this.EfectTextBox);
+            this.tabPage2.Controls.Add(this.PushToStackButton);
+            this.tabPage2.Controls.Add(this.StackListView);
+            this.tabPage2.Controls.Add(this.PopStackButton);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(811, 581);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Co je na stacku?";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // EfectTextBox
+            // 
+            this.EfectTextBox.Location = new System.Drawing.Point(6, 58);
+            this.EfectTextBox.Name = "EfectTextBox";
+            this.EfectTextBox.Size = new System.Drawing.Size(205, 20);
+            this.EfectTextBox.TabIndex = 5;
+            // 
+            // PushToStackButton
+            // 
+            this.PushToStackButton.Location = new System.Drawing.Point(6, 6);
+            this.PushToStackButton.Name = "PushToStackButton";
+            this.PushToStackButton.Size = new System.Drawing.Size(205, 46);
+            this.PushToStackButton.TabIndex = 4;
+            this.PushToStackButton.Text = "TRIGGER";
+            this.PushToStackButton.UseVisualStyleBackColor = true;
+            this.PushToStackButton.Click += new System.EventHandler(this.PushToStackButton_Click);
+            // 
+            // StackListView
+            // 
+            this.StackListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Index,
+            this.EfectNameColumn});
+            this.StackListView.GridLines = true;
+            this.StackListView.Location = new System.Drawing.Point(6, 84);
+            this.StackListView.Name = "StackListView";
+            this.StackListView.Size = new System.Drawing.Size(393, 491);
+            this.StackListView.TabIndex = 3;
+            this.StackListView.UseCompatibleStateImageBehavior = false;
+            this.StackListView.View = System.Windows.Forms.View.Details;
+            // 
+            // EfectNameColumn
+            // 
+            this.EfectNameColumn.Text = "Efect Name";
+            this.EfectNameColumn.Width = 284;
+            // 
+            // PopStackButton
+            // 
+            this.PopStackButton.Location = new System.Drawing.Point(217, 6);
+            this.PopStackButton.Name = "PopStackButton";
+            this.PopStackButton.Size = new System.Drawing.Size(182, 37);
+            this.PopStackButton.TabIndex = 1;
+            this.PopStackButton.Text = "RESOLVE";
+            this.PopStackButton.UseVisualStyleBackColor = true;
+            this.PopStackButton.Click += new System.EventHandler(this.PopStackButton_Click);
+            // 
+            // Index
+            // 
+            this.Index.Text = "Index";
+            this.Index.Width = 51;
+            // 
+            // ResolveAllButton
+            // 
+            this.ResolveAllButton.Location = new System.Drawing.Point(217, 49);
+            this.ResolveAllButton.Name = "ResolveAllButton";
+            this.ResolveAllButton.Size = new System.Drawing.Size(182, 29);
+            this.ResolveAllButton.TabIndex = 6;
+            this.ResolveAllButton.Text = "RESOLVE ALL";
+            this.ResolveAllButton.UseVisualStyleBackColor = true;
+            this.ResolveAllButton.Click += new System.EventHandler(this.ResolveAllButton_Click);
+            // 
+            // d
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(843, 631);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "d";
+            this.Text = "Medžikátor";
+            ((System.ComponentModel.ISupportInitialize)(this.PocetKaretVBaliku)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PocetLanduVBaliku)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PocetKaretVRuce)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PocetLanduVRuce)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown PocetKaretVBaliku;
+        private System.Windows.Forms.NumericUpDown PocetLanduVBaliku;
+        private System.Windows.Forms.NumericUpDown PocetKaretVRuce;
+        private System.Windows.Forms.NumericUpDown PocetLanduVRuce;
+        private System.Windows.Forms.Label a;
+        private System.Windows.Forms.Label b;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label q;
+        private System.Windows.Forms.Button VypocitatButton;
+        private System.Windows.Forms.Label vysledek1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label vysledek5;
+        private System.Windows.Forms.Label vysledek4;
+        private System.Windows.Forms.Label vysledek3;
+        private System.Windows.Forms.Label vysledek2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.CheckBox Scry;
+        private System.Windows.Forms.TextBox EfectTextBox;
+        private System.Windows.Forms.Button PushToStackButton;
+        private System.Windows.Forms.ListView StackListView;
+        private System.Windows.Forms.ColumnHeader EfectNameColumn;
+        private System.Windows.Forms.Button PopStackButton;
+        public System.Windows.Forms.ColumnHeader Index;
+        private System.Windows.Forms.Button ResolveAllButton;
+    }
+}
+
---
+++ Pocitadlo landu/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace Pocitadlo_landu
+{
+    public partial class d : Form
+    {
+        double KartyTed;
+        double LandyTed;
+
+        string nadavka = "Něco si asi posral";
+
+        public d()
+        {
+            InitializeComponent();
+        }
+
+        //------------------------------ KEEPENS? ---------------------------------------
+        private void VypocitatButton_Click(object sender, EventArgs e)
+        {
+            double[] pole = new double[6];
+
+            KartyTed = (double)PocetKaretVBaliku.Value - (double)PocetKaretVRuce.Value;
+            LandyTed = (double)PocetLanduVBaliku.Value - (double)PocetLanduVRuce.Value;
+
+            if (KartyTed < 1)
+            {
+                MessageBox.Show(nadavka);
+                return;
+            }
+
+            for (int nKolo = 0; nKolo < 6; nKolo++)
+            {
+
+                if (nKolo == 0)
+                {
+                    pole[nKolo] = 1 - (LandyTed / (KartyTed - nKolo));
+                    continue;
+                }
+
+                pole[nKolo] = (1 - (LandyTed / (KartyTed - nKolo))) * pole[nKolo - 1];
+            }
+
+            if (Scry.Checked == true)
+            {
+                vysledek1.Text = FormatPravedpodobnsotFromDouble(pole[1]);
+                vysledek2.Text = FormatPravedpodobnsotFromDouble(pole[2]);
+                vysledek3.Text = FormatPravedpodobnsotFromDouble(pole[3]);
+                vysledek4.Text = FormatPravedpodobnsotFromDouble(pole[4]);
+                vysledek5.Text = FormatPravedpodobnsotFromDouble(pole[5]);
+            }
+            else
+            {
+                vysledek1.Text = FormatPravedpodobnsotFromDouble(pole[0]);
+                vysledek2.Text = FormatPravedpodobnsotFromDouble(pole[1]);
+                vysledek3.Text = FormatPravedpodobnsotFromDouble(pole[2]);
+                vysledek4.Text = FormatPravedpodobnsotFromDouble(pole[3]);
+                vysledek5.Text = FormatPravedpodobnsotFromDouble(pole[4]);
+
+            }
+
+            return;
+        }
+        
+        private string FormatPravedpodobnsotFromDouble(double pst)
+        {
+            return ((1 - pst) * 100).ToString() + "%";
+        }
+
+        //------------------------------ STACK RESOLVER ---------------------------------------
+        private string[] Stack = new string[0];
+        private int stackSize = 0;
+        private int numberOfTriggers = 0;
+
+        private void ResolveAbility()
+        {
+            // to implement
+        }
+
+        private void PutNewTriggerOnStack(string newTrigger)
+        {
+            // to implement
+        }
+
+        private void ResolveAllOnStack()
+        {
+            // to implement
+        }
+        
+        // ------------------ Eventy UI prvku - pro Sebka: zatim ignorovat a nehrabat :D ---------------------
+        // Resolve
+        private void PopStackButton_Click(object sender, EventArgs e)
+        {
+            ResolveAbility();
+
+            UpdateListViewCollection();
+        }
+
+        // Trigger
+        private void PushToStackButton_Click(object sender, EventArgs e)
+        {
+            var efect = EfectTextBox.Text;
+
+            PutNewTriggerOnStack(efect);
+
+            UpdateListViewCollection();
+        }
+
+        private void ResolveAllButton_Click(object sender, EventArgs e)
+        {
+            ResolveAllOnStack();
+
+            UpdateListViewCollection();
+        }
+
+        private void UpdateListViewCollection()
+        {
+            StackListView.Items.Clear();
+
+            for (int i = 0; i < stackSize; i++)
+            {
+                var lvi = new ListViewItem(i.ToString());
+                
+                lvi.SubItems.Add(Stack[i]);
+                
+                StackListView.Items.Add(lvi);
+            }
+        }
+    }
+}
